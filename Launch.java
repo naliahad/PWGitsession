4de67ade9a884148155ab8d87ac85d127dfd7e7c
naliahad <ahadali2842000@gmail.com>
2023-02-26 12:35:28 +0530

    launch.java is modified

--- Launch.java
+++ Launch.java
@@ -3,5 +3,5 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        System.out.println("Hello Java lovers");
     }
